ed38df815ff6151bad5bffca6fb3ca24bdc4fc47
Marcus Jones <marcusjones8119@gmail.com>
2024-02-26 09:39:37 -0500

    kek

--- apcsp_u8/__init__.py
+++ apcsp_u8/__init__.py
@@ -1,19 +1,26 @@
 import pygame
+from datetime import datetime
 
 # set up pygame modules
 pygame.init()
 pygame.font.init()
-my_font = pygame.font.SysFont('Arial', 15)
+my_font = pygame.font.SysFont('HYWenHei-85W', 24)
 pygame.display.set_caption("AP CSP Pygame!")
 
 # set up variables for the display
-size = (400, 300)
+size = (800, 600)
 screen = pygame.display.set_mode(size)
 
-name = "Mr. Das"
+person = {
+    "name": "Issac Liu",
+    "slogan": "I don't care",
+    "date": datetime.now().strftime("%m/%d/%Y")
+}
 
 # render the text for later
-display_name = my_font.render(name, True, (255, 255, 255))
+display_name = my_font.render(person['name'], True, (255, 255, 255))
+display_slogan = my_font.render(f"Slogan: {person['slogan']}", True, (255, 255, 255))
+display_date = my_font.render(f"Date: {person['date']}", True, (255, 255, 255))
 
 # The loop will carry on until the user exits the game (e.g. clicks the close button).
 run = True
@@ -25,8 +32,10 @@ while run:
         if event.type == pygame.QUIT:  # If user clicked close
             run = False
 
-    screen.fill((0, 0, 0))
+    screen.fill((38, 35, 53))
     screen.blit(display_name, (0, 0))
+    screen.blit(display_slogan, ((size[0]//2) - 125, (size[1]//2) - 30))
+    screen.blit(display_date, (size[0] - 225, size[1] - 30))
     pygame.display.update()
 
 # Once we have exited the main program loop we can stop the game engine:
