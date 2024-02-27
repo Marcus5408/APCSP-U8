a1b80425ec215abcc5447a96f2b8def8c5e0ac04
Marcus Jones <marcusjones8119@gmail.com>
2024-02-27 09:31:58 -0500

    rtueth

--- apcsp_u8/__init__.py
+++ apcsp_u8/__init__.py
@@ -17,13 +17,41 @@ person = {
     "date": datetime.now().strftime("%m/%d/%Y")
 }
 
-# render the text for later
-display_name = my_font.render(person['name'], True, (255, 255, 255))
-display_slogan = my_font.render(f"Slogan: {person['slogan']}", True, (255, 255, 255))
-display_date = my_font.render(f"Date: {person['date']}", True, (255, 255, 255))
+colors = {
+    "0": {
+        "text": (0, 0, 0),
+        "background": (137, 210, 220)
+    },
+    "1": {
+        "text": (0, 0, 0),
+        "background": (101, 100, 219)
+    },
+    "2": {
+        "text": (255, 255, 255),
+        "background": (35, 46, 209)
+    },
+    "3": {
+        "text": (255, 255, 255),
+        "background": (16, 29, 66)
+    },
+    "4": {
+        "text": (255, 255, 255),
+        "background": (13, 19, 23)
+    }
+}
+
+def render_text(person: dict):
+    # render the text for later
+    mod_time = str(int(datetime.now().strftime("%S")) // 2 % 5)
+    display_name = my_font.render(person['name'], True, colors[mod_time]['text'])
+    display_slogan = my_font.render(f"Slogan: {person['slogan']}", True, colors[mod_time]['text'])
+    display_date = my_font.render(f"Date: {person['date']}", True, colors[mod_time]['text'])
+
+    return display_name, display_slogan, display_date
 
 # The loop will carry on until the user exits the game (e.g. clicks the close button).
 run = True
+time_started = datetime.now()
 
 # -------- Main Program Loop -----------
 while run:
@@ -32,7 +60,14 @@ while run:
         if event.type == pygame.QUIT:  # If user clicked close
             run = False
 
-    screen.fill((38, 35, 53))
+    screen.fill(colors[str(int(datetime.now().strftime("%S")) // 2 % 5)]["background"])
+    display_name, display_slogan, display_date = render_text(person)
+    time_elapsed = datetime.now() - time_started
+    time_elapsed = time_elapsed.total_seconds()
+    time_elapsed = round(time_elapsed, 3)
+    time_elapsed = f"{time_elapsed} seconds"
+    display_time_elapsed = my_font.render(f"Time Elapsed: {time_elapsed}", True, colors[str(int(datetime.now().strftime("%S")) // 2 % 5)]["text"])
+    screen.blit(display_time_elapsed, (0, size[1] - 30))
     screen.blit(display_name, (0, 0))
     screen.blit(display_slogan, ((size[0]//2) - 100, (size[1]//2) - 30))
     screen.blit(display_date, (size[0] - 225, size[1] - 30))
